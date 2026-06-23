ab91b1793eaa9344f7965e055e9341fe5eefa118
Krishan kumar <krishankumar@Krishans-MacBook-Air.local>
2026-06-23 14:41:44 +0530

    Added weather app UI and API integration

--- Submissions/Weather_api.js
+++ Submissions/Weather_api.js
@@ -1,15 +1,25 @@
 const apiKey = "c8e5d25715312096700004f297cb415c";
-const city = "Delhi";
 
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+async function getWeather() {
 
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-    console.log("City:", data.name);
-    console.log("Temperature:", data.main.temp + " °C");
-    console.log("Humidity:", data.main.humidity + "%");
-    console.log("Weather:", data.weather[0].description);
-  })
-  .catch(error => console.error(error));
+    const city = document.getElementById("city").value;
+
+    const url =
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    try {
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        document.getElementById("result").innerHTML = `
+            <h3>${data.name}</h3>
+            <p>Temperature: ${data.main.temp} °C</p>
+            <p>Humidity: ${data.main.humidity}%</p>
+            <p>Weather: ${data.weather[0].description}</p>
+        `;
+
+    } catch (error) {
+        console.log(error);
+    }
+}
